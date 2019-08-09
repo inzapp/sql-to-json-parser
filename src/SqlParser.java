c8b9c695cec658e0df2e79521e61b836c8a19739
inzapp <keg9903@naver.com>
2019-08-09 13:32:17 +0900

    parse test 예제 첨부

--- src/SqlParser.java
+++ src/SqlParser.java
@@ -1,7 +1,17 @@
 import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.expression.BinaryExpression;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.ExpressionVisitorAdapter;
+import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
+import net.sf.jsqlparser.expression.operators.conditional.OrExpression;
+import net.sf.jsqlparser.expression.operators.relational.Between;
+import net.sf.jsqlparser.expression.operators.relational.ComparisonOperator;
+import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
+import net.sf.jsqlparser.expression.operators.relational.MultiExpressionList;
 import net.sf.jsqlparser.parser.CCJSqlParser;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.*;
 import net.sf.jsqlparser.statement.alter.Alter;
 import net.sf.jsqlparser.statement.comment.Comment;
@@ -15,10 +25,7 @@ import net.sf.jsqlparser.statement.execute.Execute;
 import net.sf.jsqlparser.statement.insert.Insert;
 import net.sf.jsqlparser.statement.merge.Merge;
 import net.sf.jsqlparser.statement.replace.Replace;
-import net.sf.jsqlparser.statement.select.AllColumns;
-import net.sf.jsqlparser.statement.select.PlainSelect;
-import net.sf.jsqlparser.statement.select.Select;
-import net.sf.jsqlparser.statement.select.SelectItem;
+import net.sf.jsqlparser.statement.select.*;
 import net.sf.jsqlparser.statement.truncate.Truncate;
 import net.sf.jsqlparser.statement.update.Update;
 import net.sf.jsqlparser.statement.upsert.Upsert;
@@ -28,32 +35,8 @@ import net.sf.jsqlparser.util.TablesNamesFinder;
 import java.util.*;
 
 public class SqlParser {
-    private static String SQL = "SELECT  \n" +
-            "         EC_CLPS_DV_CD         as   STAFF_GBN,            \n" +
-            "         EC_NM            as   STAFF_CLASS,          \n" +
-            "         EC_GRP            as   ORG_STAFF_GBN,        \n" +
-            "         ODR            as   ORDER_SEQ,            \n" +
-            "         ORGZ_ID            as   ORG_ID,\n" +
-            "         EMP_NO            as   STAFF_EMP_ID,         \n" +
-            "         USE_AYN            as   USE_YN,         \n" +
-            "         REG_DTM,                          \n" +
-            "         RGR_ID,         \n" +
-            "         UPDT_DTM,       \n" +
-            "         UTUR_ID\n" +
-            "      FROM ETCH005M\n" +
-            "         WHERE EC_CLPS_DV_CD IN (\n" +
-            "                  SELECT   EC_DV as staff_gbn\n" +
-            "                      FROM ETCH004M\n" +
-            "                     WHERE (TO_DATE (SCH_REG_DT , 'YYYY-MM-DD')\n" +
-            "                               BETWEEN TO_DATE (SYSDATE, 'YYYY-MM-DD')\n" +
-            "                                   AND TO_DATE (SYSDATE, 'YYYY-MM-DD')\n" +
-            "                           )\n" +
-            "                  GROUP BY EC_DV \n" +
-            "                  UNION\n" +
-            "                  SELECT EC_CLPS_DV_CD as staff_gbn\n" +
-            "                    FROM ETCH005M\n" +
-            "                   WHERE NVL (USE_AYN, 'Y') <> 'N'   )\n" +
-            "      ORDER BY ODR";
+    private static String SQL = "SELECT * FROM TAB where a = 1 and (b = 2 and c = 3) or d = 4";
+//    private static String SQL = "SELECT * FROM TAB where a between c and d";
 
     private static final String[] SYNTAX = {
             "SELECT", "DISTINCT", "INSERT", "UPDATE", "DELETE", "FROM", "WHERE",
@@ -78,20 +61,71 @@ public class SqlParser {
         return tableList;
     }
 
+    private static List<String> getSelectWhereList(Statement statement) {
+        Select selectStatement = (Select) statement;
+        PlainSelect plainSelect = (PlainSelect) selectStatement.getSelectBody();
+        Expression whereExpression = plainSelect.getWhere();
+        whereExpression.accept(new ExpressionVisitorAdapter() {
+            @Override
+            public void visit(MultiExpressionList multiExprList) {
+                for(ExpressionList expression : multiExprList.getExprList()) {
+                    System.out.println(expression.toString());
+                }
+                super.visit(multiExprList);
+            }
+
+            @Override
+            protected void visitBinaryExpression(BinaryExpression expr) {
+                if(expr instanceof ComparisonOperator) {
+                    System.out.println(expr.getLeftExpression() + expr.getStringExpression() + expr.getRightExpression());
+                }
+                super.visitBinaryExpression(expr);
+            }
+        });
+//        System.out.println(plainSelect.getWhere().toString());
+//        net.sf.jsqlparser.expression.operators.relational.
+//        if(whereExpression instanceof AndExpression) {
+//            System.out.println("and");
+//        } else if(whereExpression instanceof OrExpression) {
+//            System.out.println("or");
+//        } else if(whereExpression instanceof Between){
+//            System.out.println("between");
+//        } else {
+//            System.out.println("else");
+//        }
+        return new ArrayList<>();
+    }
+
     public static void main(String[] args) {
-        Statement statement;
         try {
-            statement = CCJSqlParserUtil.parse(SQL);
+            ParseTest.main(new String[]{});
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.exit(1);
+        Select statement;
+        try {
+            statement = (Select) CCJSqlParserUtil.parse(SQL);
         } catch (JSQLParserException e) {
             e.printStackTrace();
             return;
         }
 
-        for (String cur : getSelectColumnList(statement))
-            System.out.println(cur);
+        getSelectWhereList(statement);
+//        PlainSelect ps = (PlainSelect) statement.getSelectBody();
+//        System.out.println(ps.getWhere().toString());
+//        System.out.println(ps.getSelectItems().get(0).toString());
 
-        for (String cur : getSelectTableList(statement))
-            System.out.println(cur);
+        // here you have to check what kind of expression it is and execute your actions individualy for every expression implementation
+//        AndExpression e = (AndExpression) ps.getWhere();
+//        System.out.println(e.getLeftExpression());
+
+//        for (String cur : getSelectColumnList(statement))
+//            System.out.println(cur);
+//
+//        for (String cur : getSelectTableList(statement))
+//            System.out.println(cur);
+
         System.exit(1);
 
         List<String> strList = new LinkedList<>(Arrays.asList(SQL.split(" ")));
@@ -205,5 +239,154 @@ public class SqlParser {
             untilStrList.add(cur.trim());
         }
         return getString(untilStrList);
+    }
+}
+
+/**
+ * Created by 2YSP on 2019/7/6.
+ */
+class ParseTest {
+
+    public static final String SELECT_SQL = "select user_name,age,email from t_user " +
+            "where user_id > 16546 group by age order by user_name desc";
+
+    public static final String INSERT_SQL = "insert into t_order (id,user_id,sum) values ('EF1243',12,23.6)";
+
+    public static final String UPDATE_SQL = "update person set first_name = 'Fred' where last_name ='Wilson'";
+
+    public static final String DELETE_SQL = "delete from t_item where id = 'AF3434'";
+
+    public static void main(String[] args) throws Exception {
+        ParseTest.parseSQL(SELECT_SQL);
+        ParseTest.parseSQL(INSERT_SQL);
+        ParseTest.parseSQL(UPDATE_SQL);
+        ParseTest.parseSQL(DELETE_SQL);
+
+    }
+
+    public static void parseSQL(String sql) throws JSQLParserException {
+        Statement statement = CCJSqlParserUtil.parse(sql);
+        System.out.println("\n==============sql: " + sql);
+        if (statement instanceof Select) {
+            Select select = (Select) statement;
+            parseSelect(select);
+        }
+        if (statement instanceof Update) {
+            Update update = (Update) statement;
+            parseUpdate(update);
+        }
+        if (statement instanceof Insert) {
+            Insert insert = (Insert) statement;
+            parseInsert(insert);
+        }
+        if (statement instanceof Delete) {
+            Delete delete = (Delete) statement;
+            parseDelete(delete);
+        }
+    }
+
+    private static void parseDelete(Delete delete) {
+        //解析SQL中的表名
+        System.out.print("\n表名: ");
+        Table table = delete.getTable();
+        System.out.print(table.getName());
+        //解析SQL中的Where部分
+        Expression where = delete.getWhere();
+        System.out.print("\nWhere部分: " + where.toString());
+    }
+
+    private static void parseInsert(Insert insert) {
+        // 获取更新的列名
+        System.out.print("\n列名: ");
+        List<Column> columns = insert.getColumns();
+        if (columns != null) {
+            columns.forEach(column -> System.out.print(column.getColumnName() + " "));
+        }
+        // 解析表名
+        System.out.print("\n表名: ");
+        String tableName = insert.getTable().getName();
+        System.out.print(tableName);
+
+        // 解析insert语句中的插入记录的各个列值
+        System.out.print("\n列值:");
+        List<Expression> insertValueExpressionList = ((ExpressionList) insert.getItemsList())
+                .getExpressions();
+        insertValueExpressionList.forEach(expression -> System.out.print(expression.toString() + " "));
+        System.out.println();
+    }
+
+    private static void parseUpdate(Update update) {
+        //解析列名
+        System.out.print("\n列名: ");
+        List<Column> columns = update.getColumns();
+        if (columns != null) {
+            columns.forEach(column -> System.out.print(column.getColumnName() + " "));
+        }
+        // 解析表名
+        System.out.print("\n表名: ");
+        List<Table> tables = update.getTables();
+        tables.forEach(table -> System.out.print(table.getName() + " "));
+        // 解析 列值
+        System.out.print("\n列值: ");
+        List<Expression> expressions = update.getExpressions();
+        expressions.forEach(expression -> System.out.print(expression.toString() + " "));
+        // 解析where部分
+        Expression whereExpression = update.getWhere();
+        System.out.print("\nWhere部分: " + whereExpression);
+        System.out.println();
+    }
+
+    private static void parseSelect(Select select) {
+        // 获取select语句查询的列
+        System.out.print("\n列名: ");
+        PlainSelect plain = (PlainSelect) select.getSelectBody();
+        List<SelectItem> selectItems = plain.getSelectItems();
+        if (selectItems != null) {
+            for (int i = 0; i < selectItems.size(); i++) {
+                SelectItem selectItem = selectItems.get(i);
+                System.out.print(selectItem.toString() + " ");
+            }
+        }
+        // 解析Select语句中的表名
+        System.out.print("\n表名: ");
+        TablesNamesFinder tablesNamesFinder = new TablesNamesFinder();
+        List<String> tableList = tablesNamesFinder.getTableList(select);
+        tableList.forEach(tableName -> System.out.print(tableName));
+
+        // 解析SQL语句中的where 部分
+        Expression whereExpression = plain.getWhere();
+        System.out.print("\nWhere部分: " + whereExpression.toString());
+        // 解析SQL语句中的group by 部分
+        System.out.print("\nGroup by 部分的列名: ");
+        GroupByElement groupByElement = plain.getGroupBy();
+        List<Expression> groupByExpressions = groupByElement.getGroupByExpressions();
+        if (groupByExpressions != null) {
+            groupByExpressions
+                    .forEach(groupByExpression -> System.out.print(groupByExpression.toString()));
+        }
+
+        // 解析SQL语句中的 order by 部分的列名
+        System.out.print("\norder by 部分的列名: ");
+        List<OrderByElement> orderByElementList = plain.getOrderByElements();
+        if (orderByElementList != null) {
+            orderByElementList
+                    .forEach(orderByElement -> System.out.print(orderByElement.getExpression().toString()));
+        }
+        System.out.println();
+    }
+
+    public static void parseSelectJoin(String sql) throws JSQLParserException {
+        Statement statement = CCJSqlParserUtil.parse(sql);
+        Select select = (Select) statement;
+        PlainSelect plainSelect = (PlainSelect) select.getSelectBody();
+        List<Join> joinList = plainSelect.getJoins();
+        if (joinList != null) {
+            for (int i = 0; i < joinList.size(); i++) {
+                Join join = joinList.get(i);
+                System.out.println("JOIN部分: " + join.toString());
+                System.out.println("链接表达式: " + join.getOnExpression().toString());
+            }
+        }
+
     }
 }
