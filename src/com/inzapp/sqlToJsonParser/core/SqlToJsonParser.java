e3ef973028f238687ded868d46969e92856e46aa
inzapp <keg9903@naver.com>
2019-09-06 09:26:41 +0900

    lib

--- src/com/inzapp/sqlToJsonParser/core/SqlToJsonParser.java
+++ src/com/inzapp/sqlToJsonParser/core/SqlToJsonParser.java
@@ -1,47 +1,22 @@
 package com.inzapp.sqlToJsonParser.core;
 
-import com.inzapp.sqlToJsonParser.config.Config;
-import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 
 public class SqlToJsonParser {
     /**
-     * main method
-     * used for executable jar
+     * used for java code
      *
-     * @param args not used
+     * @param sql raw sql query
+     * @return parsed json string
+     * return null if exception was caught
      */
-    public static void main(String[] args) {
-        String inputFileName = Config.INPUT_FILE_NAME;
-        String outputFileName = Config.OUTPUT_FILE_NAME;
-        if (args != null && args.length == 2) {
-            inputFileName = args[0];
-            outputFileName = args[1];
-        }
-
-        SqlToJsonParser sqlToJsonParser = new SqlToJsonParser();
-        SqlVisitor sqlVisitor = new SqlVisitor();
+    public String parse(String sql) {
         try {
-            String sql = sqlToJsonParser.readSqlFromFile(inputFileName);
-            if (sql == null)
-                throw new Exception("input file does not exist");
-
-            JSONObject json = sqlVisitor.parse(sql);
-            if (json == null)
-                throw new Exception("sql syntax error");
-
-            String jsonString = json.toString(4);
-            System.out.println("input sql\n\n" + sql);
-            System.out.println("output json\n\n" + jsonString);
-
-            sqlToJsonParser.saveFile(jsonString, outputFileName);
-            System.out.println("parse success");
+            return new SqlVisitor().parse(sql).toString(4);
         } catch (Exception e) {
-            sqlToJsonParser.saveFile(Config.SQL_SYNTAX_ERROR, outputFileName);
-            System.out.println(e.getMessage());
+            return null;
         }
     }
 
