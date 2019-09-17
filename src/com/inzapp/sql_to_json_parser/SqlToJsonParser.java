bbfe074cdc9e5e777719c74b30eddd58ebc814a7
inzapp <keg9903@naver.com>
2019-09-17 11:05:45 +0900

    jar import를 위한 refactor 중

--- src/com/inzapp/sql_to_json_parser/SqlToJsonParser.java
+++ src/com/inzapp/sql_to_json_parser/SqlToJsonParser.java
@@ -7,59 +7,90 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class SqlToJsonParser {
+//    /**
+//     * main method
+//     * used for executable jar
+//     *
+//     * @param args [0] : specified input file name
+//     *             [1] : specified output file name
+//     */
+//    public static void main(String[] args) {
+//        String inputFileName = Config.INPUT_FILE_NAME;
+//        String outputFileName = Config.OUTPUT_FILE_NAME;
+//        if (args != null && args.length == 2) {
+//            inputFileName = args[0];
+//            outputFileName = args[1];
+//        }
+//
+//        SqlToJsonParser sqlToJsonParser = new SqlToJsonParser();
+//        Parser parser = new Parser();
+//        try {
+//            String sql = sqlToJsonParser.readSqlFromFile(inputFileName);
+//            if (sql == null)
+//                throw new Exception("input file does not exist");
+//
+//            JSONObject json = parser.parse(sql);
+//            if (json == null)
+//                throw new Exception("sql syntax error");
+//
+//            String jsonString = json.toString(4);
+//            System.out.println("input sql\n\n" + sql);
+//            System.out.println("output json\n\n" + jsonString);
+//
+//            sqlToJsonParser.saveFile(jsonString, outputFileName);
+//            System.out.println("parse success");
+//        } catch (Exception e) {
+//            sqlToJsonParser.saveFile(Config.SQL_SYNTAX_ERROR, outputFileName);
+//            System.out.println(e.getMessage());
+//        }
+//    }
+
     /**
-     * main method
-     * used for executable jar
+     * used for java code
      *
-     * @param args [0] : specified input file name
-     *             [1] : specified output file name
+     * @param sql raw sql query encoded with base 64
+     * @return parsed json string
+     * return null if exception was caught
      */
-    public static void main(String[] args) {
-        String inputFileName = Config.INPUT_FILE_NAME;
-        String outputFileName = Config.OUTPUT_FILE_NAME;
-        if (args != null && args.length == 2) {
-            inputFileName = args[0];
-            outputFileName = args[1];
-        }
-
-        SqlToJsonParser sqlToJsonParser = new SqlToJsonParser();
-        Parser parser = new Parser();
-        try {
-            String sql = sqlToJsonParser.readSqlFromFile(inputFileName);
-            if (sql == null)
-                throw new Exception("input file does not exist");
-
-            JSONObject json = parser.parse(sql);
-            if (json == null)
-                throw new Exception("sql syntax error");
-
-            String jsonString = json.toString(4);
-            System.out.println("input sql\n\n" + sql);
-            System.out.println("output json\n\n" + jsonString);
-
-            sqlToJsonParser.saveFile(jsonString, outputFileName);
-            System.out.println("parse success");
-        } catch (Exception e) {
-            sqlToJsonParser.saveFile(Config.SQL_SYNTAX_ERROR, outputFileName);
-            System.out.println(e.getMessage());
-        }
+    public String parse(String sql) {
+        return parse(sql, true);
     }
 
     /**
      * used for java code
      *
      * @param sql raw sql query
+     * @param isEncoded64 true if encoded with base 64
      * @return parsed json string
      * return null if exception was caught
      */
-    public String parse(String sql) {
+    public String parse(String sql, boolean isEncoded64) {
+        if(isEncoded64)
+            sql = decode64(sql);
+
         try {
             return new Parser().parse(sql).toString(4);
         } catch (Exception e) {
             return null;
         }
+    }
+
+    private String encode64(String raw) {
+        byte[] bytes = raw.getBytes(StandardCharsets.UTF_8);
+        Base64.Encoder encoder = Base64.getEncoder();
+        byte[] encodedBytes = encoder.encode(bytes);
+        return encoder.encodeToString(encodedBytes);
+    }
+
+    private String decode64(String encoded64) {
+        byte[] encodedBytes = encoded64.getBytes();
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] decodedBytes = decoder.decode(encodedBytes);
+        return new String(decodedBytes, StandardCharsets.UTF_8);
     }
 
     /**
